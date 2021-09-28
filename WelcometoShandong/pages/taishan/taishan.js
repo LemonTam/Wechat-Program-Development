68693e47a3f5419a982c2aa3709f0867e9f3cacf
唐一一 <91330210+LemonTam@users.noreply.github.com>
2021-09-28 23:19:47 +0800

    好客山东欢迎您！
    好客山东小程序，五个导航按钮分别为：
    •首页：好客山东欢迎您，山东的介绍。
    •泰山：泰山介绍 
    •泉城：泉城介绍
    •三孔：三孔介绍 
    •家乡：我的家乡-东平介绍

---
+++ WelcometoShandong/pages/taishan/taishan.js
@@ -0,0 +1,10 @@
+// pages/taishan/taishan.js
+Page({
+    data:{
+        ttt1:'/images/tx1.jpg',
+        ttt2:'/images/tx2.jpg',
+        ttt3:'/images/tx3.jpg',
+        ttt4:'/images/tx4.jpg',
+        ttt5:'/images/tx5.jpg'
+    }
+})
